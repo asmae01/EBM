d1d250c323c469481cf037fb9764d4aa85c98da7
asmae01 <asmaeoubraim331999@gmail.com>
2023-11-21 14:34:10 +0100

    first commit

---
+++ app/routes/($locale).about.jsx
@@ -0,0 +1,33 @@
+export function about() {  
+   
+      return (
+        <div>
+          <h1>about</h1>
+          <p>Status: </p>
+          <div>
+          <h1>about</h1>
+          <p>Status: </p>
+        </div>  <div>
+          <h1>about</h1>
+          <p>Status: </p>
+        </div>  <div>
+          <h1>about</h1>
+          <p>Status: </p>
+        </div>  <div>
+          <h1>about</h1>
+          <p>Status: </p>
+        </div>  <div>
+          <h1>about</h1>
+          <p>Status: </p>
+        </div>  <div>
+          <h1>about</h1>
+          <p>Status: </p>
+        </div>  <div>
+          <h1>about</h1>
+          <p>Status: </p>
+        </div>
+        </div>
+        
+      );
+    
+}  
---
+++ app/routes/($locale).account_.login.jsx
@@ -0,0 +1,159 @@
+import {json, redirect} from '@shopify/remix-oxygen';
+import {Form, Link, useActionData} from '@remix-run/react';
+
+/**
+ * @type {MetaFunction}
+ */
+export const meta = () => {
+  return [{title: 'Login'}];
+};
+
+/**
+ * @param {LoaderFunctionArgs}
+ */
+export async function loader({context}) {
+  if (await context.session.get('customerAccessToken')) {
+    return redirect('/account');
+  }
+  return json({});
+}
+
+/**
+ * @param {ActionFunctionArgs}
+ */
+export async function action({request, context}) {
+  const {session, storefront} = context;
+
+  if (request.method !== 'POST') {
+    return json({error: 'Method not allowed'}, {status: 405});
+  }
+
+  try {
+    const form = await request.formData();
+    const email = String(form.has('email') ? form.get('email') : '');
+    const password = String(form.has('password') ? form.get('password') : '');
+    const validInputs = Boolean(email && password);
+
+    if (!validInputs) {
+      throw new Error('Please provide both an email and a password.');
+    }
+
+    const {customerAccessTokenCreate} = await storefront.mutate(
+      LOGIN_MUTATION,
+      {
+        variables: {
+          input: {email, password},
+        },
+      },
+    );
+
+    if (!customerAccessTokenCreate?.customerAccessToken?.accessToken) {
+      throw new Error(customerAccessTokenCreate?.customerUserErrors[0].message);
+    }
+
+    const {customerAccessToken} = customerAccessTokenCreate;
+    session.set('customerAccessToken', customerAccessToken);
+
+    return redirect('/account', {
+      headers: {
+        'Set-Cookie': await session.commit(),
+      },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return json({error: error.message}, {status: 400});
+    }
+    return json({error}, {status: 400});
+  }
+}
+
+export default function Login() {
+  /** @type {ActionReturnData} */
+  const data = useActionData();
+  const error = data?.error || null;
+
+  return (
+    <div className="col-lg-6">
+      <h1>Login</h1>
+      <div class="theme-card">
+        <Form method="POST" className="theme-form">
+          <fieldset>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input 
+              type="email" 
+                className="form-control" 
+                id="email" 
+                placeholder="Email address" 
+                autoComplete="email"
+                aria-label="Email"
+                autoFocus
+                required/>
+            </div>
+            <div class="form-group">
+              <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              placeholder="Enter your password"
+              aria-label="Password"
+              minLength={8}
+              required
+            />
+            </div>
+          </fieldset>
+          {error ? (
+            <p>
+              <mark>
+                <small>{error}</small>
+              </mark>
+            </p>
+          ) : (
+            <br />
+          )}
+          <button type="submit" className='btn btn-solid'>Login</button>
+        </Form>
+      </div>
+      <br />
+      <div>
+        <p>
+          <Link to="/account/recover">Forgot password →</Link>
+        </p>
+        <p>
+          <Link to="/account/register">Register →</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+// NOTE: https://shopify.dev/docs/api/storefront/latest/mutations/customeraccesstokencreate
+const LOGIN_MUTATION = `#graphql
+  mutation login($input: CustomerAccessTokenCreateInput!) {
+    customerAccessTokenCreate(input: $input) {
+      customerUserErrors {
+        code
+        field
+        message
+      }
+      customerAccessToken {
+        accessToken
+        expiresAt
+      }
+    }
+  }
+`;
+
+/**
+ * @typedef {{
+ *   error: string | null;
+ * }} ActionResponse
+ */
+
+/** @typedef {import('@shopify/remix-oxygen').ActionFunctionArgs} ActionFunctionArgs */
+/** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
+/** @template T @typedef {import('@remix-run/react').MetaFunction<T>} MetaFunction */
+/** @typedef {import('@shopify/remix-oxygen').SerializeFrom<typeof loader>} LoaderReturnData */
+/** @typedef {import('@shopify/remix-oxygen').SerializeFrom<typeof action>} ActionReturnData */
